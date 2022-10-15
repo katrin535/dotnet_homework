1a33212446710d0089759d1ddeed56fdc2015257
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-15 13:36:01 +0400

    Решение задачи 19

--- Homework3/Task19/Program.cs
+++ Homework3/Task19/Program.cs
@@ -7,3 +7,16 @@
 12821 -> да
 
 23432 -> да*/
+
+    Console.Write("Введите пятизначное число: ");
+        int a = int.Parse(Console.ReadLine());
+        int palindrom = 0, n = a;
+        while (a > 0) 
+        {
+            palindrom = palindrom * 10 + a % 10; 
+            a /= 10;
+        }
+        Console.WriteLine(n == palindrom ? "Число является палиндромом" : "Число не является палиндромом");
+
+
+    
