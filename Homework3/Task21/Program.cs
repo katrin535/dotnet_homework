4aa6cba3b05eabd18163454659a57286818cc57a
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-15 14:20:03 +0400

    Решена задача 21

--- Homework3/Task21/Program.cs
+++ Homework3/Task21/Program.cs
@@ -5,3 +5,23 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+//-7-8 ^2 + 1-6 ^2 + 2-3 ^2
+
+Console.Write("Введите координату x1 для А: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y1 для А: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z1 для А: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x1 для B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y1 для B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z1 для B: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double x = Math.Pow((x2 - x1), 2);
+double y = Math.Pow((y2 - y1), 2);
+double z = Math.Pow((z2 - z1), 2);
+double sqrtsum = Math.Round(Math.Sqrt (x + y + z), 2);
+Console.WriteLine("Расстояние между точками = " + sqrtsum);
