4fc937d9ab6c2f0d8e45cdbd67e1aa0365862725
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-16 13:32:25 +0400

    Решена задача 27

--- Homework4/Task27/Program.cs
+++ Homework4/Task27/Program.cs
@@ -5,3 +5,24 @@
 82 -> 10
 
 9012 -> 12*/
+
+int summa (int a)
+{
+double st = 0;
+double n = Convert.ToDouble(a);
+
+while (n > 0)
+ {
+    st = st + n%10;
+    n /= 10;
+ }
+return Convert.ToInt32(st);
+}
+
+Console.Write("Введите число: ");
+int str = Convert.ToInt32(Console.ReadLine());
+int s = summa (str -1);
+Console.WriteLine("Сумма цифр в числе : " + s);
+
+
+
