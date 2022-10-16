adb67ebd23c1636ad253a96b6d259a3abc232ac7
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-16 14:55:59 +0400

    Решена задача 29

--- Homework4/Task29/Program.cs
+++ Homework4/Task29/Program.cs
@@ -3,3 +3,43 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 6, 1, 33 -> [6, 1, 33]*/
+
+int[] ArrayMethod(string numbers)
+{ 
+int[] arrayA = new int[1];
+ int j = 0;
+ for (int i = 0; i < numbers.Length; i+=1)
+ {
+    string nunmersA = "";
+
+    while (i < numbers.Length)
+    {
+      nunmersA += numbers[i];
+    
+      i+=1;
+    }
+    arrayA[j] = Convert.ToInt32(nunmersA);  
+    j+=1;
+  }
+  return arrayA;
+}
+
+void Screen(int[] a)
+{
+  int n = a.Length;
+  int i = 0;
+  Console.Write("[");
+  while(i < n)
+  {
+    Console.Write(a[i]);
+    i+=1; 
+  }
+  Console.Write("]");
+} 
+
+Console.Write("Введите элементы массива : ");
+string seriesOfNumbers = Console.ReadLine();
+int[] arrayOfNumbers = ArrayMethod(seriesOfNumbers);
+Screen(arrayOfNumbers);
+
+ 
