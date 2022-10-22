65b4eb681440a84b77ecc68138dd1dfca2caa706
Лютикова Екатерина Васильевна <lyutikovaev@intranet.vdcom>
2022-10-22 19:15:55 +0400

    Решена задача 41

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write("Введите элементы через запятую: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i+=1)
+{
+    if (arr[i] > 0)
+    {
+        count+=1;
+    }
+}
+ 
+Console.WriteLine($"Количество элементов больше 0: {count}");
